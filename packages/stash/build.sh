4f2a2c35a48b0f56d1e7ece11eb3801b167883c7
aicynide <97217381+aicynide@users.noreply.github.com>
2025-03-14 02:18:47 +0530

    ss

--- packages/stash/build.sh
+++ packages/stash/build.sh
@@ -12,6 +12,7 @@ TERMUX_PKG_HOSTBUILD=true
 termux_step_host_build() {
 	termux_setup_nodejs
 
+	npm install --global yarn
 	cp -r $TERMUX_PKG_SRCDIR/ui/v2.5 ./ui-v2.5
 	cd ui-v2.5
 	yarn install --frozen-lockfile
