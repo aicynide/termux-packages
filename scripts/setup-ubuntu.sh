520fb2be26c97ba002033a6f40e8ed0ee1f47190
aicynide <97217381+aicynide@users.noreply.github.com>
2025-03-14 02:04:19 +0530

    Update setup-ubuntu.sh

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -319,6 +319,9 @@ PACKAGES+=" tree-sitter-cli"
 # Needed by wlroots
 PACKAGES+=" glslang-tools"
 
+# Needed by stash
+PACKAGES+=" node-yarnpkg"
+
 # Do not require sudo if already running as root.
 SUDO="sudo"
 if [ "$(id -u)" = "0" ]; then
