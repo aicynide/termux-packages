702c88a0fa4e67087d8910be34ff5a4dca0d4d44
aicynide <97217381+aicynide@users.noreply.github.com>
2025-03-15 01:23:34 +0530

    Update build.sh

--- packages/stash/build.sh
+++ packages/stash/build.sh
@@ -12,28 +12,43 @@ TERMUX_PKG_HOSTBUILD=true
 termux_step_host_build() {
 	termux_setup_nodejs
 
-	npm install --global yarn
-	cp -r $TERMUX_PKG_SRCDIR/ui/v2.5 ./ui-v2.5
-	cd ui-v2.5
-	yarn install --frozen-lockfile
-	yarn build
+	cp -r $TERMUX_PKG_SRCDIR ./stash
+	mkdir -p stash/ui/v2.5/build
+	cd stash/ui/v2.5
+	export VITE_APP_DATE=$(date +%Y-%m-%d)
+	export VITE_APP_GITHASH=$(shell git rev-parse --short HEAD 2>/dev/null || echo unknown)
+	export VITE_APP_STASH_VERSION=v${TERMUX_PKG_VERSION}
+	yarnpkg install --frozen-lockfile
+	touch build/index.html
+	cd ../..
+	go generate ./cmd/stash
+	cd ui/v2.5
+	yarnpkg run gqlgen
+	yarnpkg build
 }
 
 termux_step_pre_configure() {
-	cp -r $TERMUX_PKG_HOSTBUILD_DIR/ui-v2.5/build $TERMUX_PKG_SRCDIR/web/
+	cp -r $TERMUX_PKG_HOSTBUILD_DIR/stash $TERMUX_PKG_SRCDIR/stash
 	termux_setup_golang
 }
 
 termux_step_make() {
+	cd stash
 	export CGO_ENABLED=1
-	go build -o stash -trimpath -ldflags="-s -w -extldflags=-static-pie \
+	go build -o stash -v \
+	-tags "sqlite_stat4 sqlite_math_functions" \
+	-buildmode=pie \
+	-trimpath \
+	-ldflags="-s -w -linkmode=external \
 	-X 'github.com/stashapp/stash/internal/build.buildstamp=$(date +%Y-%m-%d)' \
-	-X 'github.com/stashapp/stash/internal/build.githash=$(git rev-parse --short HEAD 2>/dev/null || echo unknown)' \
-	-X 'github.com/stashapp/stash/internal/build.version=${TERMUX_PKG_VERSION}' \
+	-X 'github.com/stashapp/stash/internal/build.githash=$(shell git rev-parse --short HEAD 2>/dev/null || echo unknown)' \
+	-X 'github.com/stashapp/stash/internal/build.version=v${TERMUX_PKG_VERSION}' \
 	-X 'github.com/stashapp/stash/internal/build.officialBuild=false'" \
+	-mod=readonly \
+	-modcacherw \
 	./cmd/stash
 }
 
 termux_step_make_install() {
-	install -Dm700 -t "${TERMUX_PREFIX}"/bin "$TERMUX_PKG_SRCDIR"/stash
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin "$TERMUX_PKG_SRCDIR"/stash/stash
 }
